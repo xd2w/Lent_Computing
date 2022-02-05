e95ad257c4327e761d9cc9069909c8c390277cdd
Devamitra Acharya <da533@cam.ac.uk>
2022-02-05 17:59:10 +0000

    Added 2 function

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -24,3 +24,30 @@ def stations_within_radius(stations, centre, r):
         if distance <= r:
             list_of_stations.append(station)
     return(list_of_stations)
+
+def rivers_with_station(stations):
+    """returns the list rivers where the stations are located"""
+
+    river_list = set([])
+    for each_station in stations :
+        river_list.add(each_station.river)
+
+    return list(river_list)
+
+
+def stations_by_river(stations):
+    """returns a dictionary where key is the name of river 
+    and the the item is a list of stations on that river"""
+
+    river_dict = dict([])
+
+    for station in stations :
+        keys = river_dict.keys()
+
+        if station.river in keys:
+            river_dict[station.river].append(station)
+
+        else:
+            river_dict[station.river] = [station]
+
+    return river_dict

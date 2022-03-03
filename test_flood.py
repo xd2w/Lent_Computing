3587f5391f7041458cbbe3c153d9ce9d5989df35
Yiheng Liu <yl827@cam.ac.uk>
2022-03-03 14:00:38 +0000

    Fixed tests

--- test_flood.py
+++ test_flood.py
@@ -1,12 +1,15 @@
 from floodsystem.flood import stations_highest_rel_level, stations_level_over_threshold
 from floodsystem.stationdata import build_station_list
+from floodsystem.stationdata import update_water_levels
 
 def test_stations_highest_rel_level():
     station_list = build_station_list()
+    update_water_levels(station_list)
     highest_list = stations_highest_rel_level(station_list, 10)
     assert(highest_list[1][1] != None)
 
 def test_stations_level_over_threshold():
     station_list = build_station_list()
-    list_over_threshold = stations_level_over_threshold(station_list, 0)
+    update_water_levels(station_list)
+    list_over_threshold = stations_level_over_threshold(station_list, 0.1)
     assert(list_over_threshold[1] != None)
